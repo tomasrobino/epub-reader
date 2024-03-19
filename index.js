bb9cc22fd8a909d42dea394c3d4d9c819f599abd
tomasrobino <tomaserobino@gmail.com>
2024-03-19 22:46:01 +0100

    parsing container.xml to js object

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const unzipper = require("unzipper");
 const fs = require("node:fs");
+const { XMLParser } = require("fast-xml-parser");
 
+
+//Reading container.xml
 fs.createReadStream("example.epub")
 .on("open", () => {
-    console.log("Opened epub file");
+    //console.log("Opened epub file");
 })
 .pipe(unzipper.Extract({ path: "dist" }))
 .on("close", () => {
-    console.log("Unzipped epub");
+    //console.log("Unzipped epub");
 });
+
+//Parsing container.xml
+const parser = new XMLParser({ignoreAttributes: false });
+let xmlObject = parser.parse(fs.readFileSync("dist/META-INF/container.xml"));
+console.log(xmlObject);
