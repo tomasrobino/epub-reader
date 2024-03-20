8a145cb4bc14c3f15b9af3794b4c38bc95d79ec9
tomasrobino <tomaserobino@gmail.com>
2024-03-20 20:13:54 +0100

    begin parsing content.opf

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ const fs = require("node:fs");
 const { XMLParser } = require("fast-xml-parser");
 
 
-//Extracting epub to mempory
+
 async function main() {
+    //Extracting epub to memory
     const directory = (await unzipper.Open.file('example.epub')).files;
+
+    const parser = new XMLParser({ignoreAttributes: false });
+    //Parsing content.opf
+    let contentOPF;
     for (let i = 0; i < directory.length; i++) {
         const path = directory[i].path;
-        if (path.includes("container.xml")) {
-
+        if (path.includes("content.opf")) {
+            contentOPF = parser.parse(await directory[i].buffer());
         }
     }
-  }
+    console.log(contentOPF)
+}
   
 main();
-
-//Parsing container.xml
-/*
-const parser = new XMLParser({ignoreAttributes: false });
-let xmlObject = parser.parse(fs.readFileSync("dist/META-INF/container.xml"));
-console.log(xmlObject);
-*/
