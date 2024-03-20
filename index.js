a96d1c6325804e8509c38190837ac910fec4ff6e
tomasrobino <tomaserobino@gmail.com>
2024-03-20 14:57:40 +0100

    extracting all files to results.txt

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ const fs = require("node:fs");
 const { XMLParser } = require("fast-xml-parser");
 
 
-//Reading container.xml
-fs.createReadStream("example.epub")
-.on("open", () => {
-    //console.log("Opened epub file");
-})
-.pipe(unzipper.Extract({ path: "dist" }))
-.on("close", () => {
-    //console.log("Unzipped epub");
-});
+//Extracting epub to cache file
+function extract() {
+    fs.unlink("result.txt", err => {
+        if (err) throw err;
+    });
+    let stream = fs.createWriteStream("result.txt", {flags: "a"});
+    fs.createReadStream("example.epub")
+    .pipe(unzipper.Parse())
+    .on("entry", async entry => {
+        stream.write(await entry.buffer())
+    });
+}
+
+extract()
 
 //Parsing container.xml
+/*
 const parser = new XMLParser({ignoreAttributes: false });
 let xmlObject = parser.parse(fs.readFileSync("dist/META-INF/container.xml"));
 console.log(xmlObject);
+*/
