a3429d2a59b75f912285f725317a4620666eb031
tomasrobino <tomaserobino@gmail.com>
2024-03-21 11:46:10 +0100

    renamed extract() to getFile; made it more generic; directory is opened in createWindow() now

--- main.js
+++ main.js
@@ -3,30 +3,26 @@ const { XMLParser } = require("fast-xml-parser");
 const { app, BrowserWindow } = require('electron')
 
 
-async function extract() {
-    //Extracting epub to memory
-    const directory = (await Open.file('example.epub')).files;
-
-    const parser = new XMLParser({ignoreAttributes: false });
-    //Parsing content.opf
-    let contentOPF;
+async function getFile(directory, path) {
+    //Path parameter doesn't really have to be the full path, just something that will uniquely identify the file
+    //Searching for file in directory
     for (let i = 0; i < directory.length; i++) {
-        const path = directory[i].path;
-        if (path.includes("101.xhtml")) {
-            contentOPF = (await directory[i].buffer()).toString();
+        if (directory[i].path.includes(path)) {
+            return await directory[i].buffer();
         }
     }
-    return contentOPF;
 }
 
-function createWindow(res) {
+async function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600
   })
-  win.loadURL("data:text/xml,"+res);
+  const directory = (await Open.file(process.argv[2])).files;
+  //console.log(await extract(directory))
+  win.loadURL("data:text/xml,"+(await getFile(directory, "content.opf")).toString());
 }
 
-app.whenReady().then(() => extract()).then(res => {
-  createWindow(res)
+app.whenReady().then(() => {
+  createWindow()
 })
